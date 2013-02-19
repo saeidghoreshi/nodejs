779af4ba71b1b0d5b4b366564d367270de5268b7
Ryan Goreshi <ryan.goreshi@yahoo.com>
2013-02-18 17:32:01 -0800

    strat on socket

---
+++ web/server.js
@@ -0,0 +1,25 @@
+﻿var http = require('http');
+
+var options =
+{
+    host: 'cloudcodeclub.com',
+    port: 80,
+    path: '/',
+    method: 'GET'
+}
+
+//simple request
+var req = http.request(options, function (response) {
+
+    console.log(response.statusCode);
+    //response.pipe(process.stdout);
+});
+req.end();
+
+//get request
+http.get(options, function (response) {
+
+    console.log(response.statusCode);
+    //response.pipe(process.stdout);
+});
+
---
+++ web/webserver.js
@@ -0,0 +1,16 @@
+﻿
+
+var fs = require('fs');
+var http = require('http');
+
+http.createServer(function (req, resp) {
+
+    resp.writeHead(200, { 'Content-Type': 'text/plain' });
+    
+    if (req.url === '/file.txt')
+        fs.createReadStream(__dirname + '/samplefile.txt').pipe(resp);
+    else
+        resp.end('Hello World');
+}).listen(8080);
+
+console.log('http server is running on 8080');
